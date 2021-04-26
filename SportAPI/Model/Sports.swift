232cf3e88f155e62b0165cd27c5f094607e41ca3
NeoFreya <neonblackcat19@gmail.com>
2021-04-26 21:06:06 +0700

    PFOJECT DONE

---
+++ SportAPI/Model/Sports.swift
@@ -0,0 +1,15 @@
+//
+//  Sports.swift
+//  SportAPI
+//
+//  Created by ? on 19/04/21.
+//
+
+import SwiftUI
+
+struct Sports : Identifiable{
+    var id = UUID()
+    var name : String
+    var image : String
+    var description : String
+}
